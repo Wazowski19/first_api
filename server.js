2ac8dc016c924b8470c4103fe9a5df28ea63aac4
Miguel Ángel García Martínez <ma.garmar98@gmail.com>
2022-05-02 21:35:58 -0500

    feat:endpoint que cambia a maus y nuevo endpoint con body

--- server.js
+++ server.js
@@ -3,10 +3,34 @@ import express from "express";
 
 const api = express();
 
-api.get('/top50', () =>{
-    return console.log("Hola mundo");
+api.get('/top50', (req, res) =>{
+    return res.send('1.-Gamma State, 2.-La Chona');
 });
 
+//Query con params
+
+api.get('/mayus', (req, res)=>{
+    const cadena = req.query.s;
+    console.log(cadena); 
+    if(cadena.length > 10){
+        return res.send('Error la cadena es mayor a 10 caracteres')
+    }
+    
+    return res.json({
+        mayus: cadena.toUpperCase(),
+
+    })
+});
+
+api.get('/suma', (req,res) =>{
+    //TO DO: falta configurar el req.body
+    const body = req.body;
+    console.log(body);
+
+    const a = 1;
+    const b = 2;
+    return res.send(`${a + b}`);
+})
 
 const server = http.createServer(api);
 
